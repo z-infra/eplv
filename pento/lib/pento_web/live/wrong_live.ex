85a76364beeaeac398b9e8b9ba6a3739633cd408
iury fukuda <iuryfukuda@gmail.com>
2023-10-17 13:12:08 -0300

    update guess game

---
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,89 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  def default(socket) do
+    assign(socket,
+      score: 0,
+      message: "Make a guess",
+      time: time(),
+      number: Enum.random(1..10),
+      win: false
+    )
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, default(socket)}
+  end
+
+  def win(socket, guess) do
+    {:noreply,
+     assign(socket,
+       message: "Your guess: #{guess}",
+       score: socket.assigns.score + 1,
+       time: time(),
+       win: true,
+       number: Enum.random(1..10)
+     )}
+  end
+
+  def lose(socket, guess) do
+    {:noreply,
+     assign(socket,
+       message: "Your guess: #{guess}",
+       score: socket.assigns.score - 1,
+       time: time(),
+       win: false
+     )}
+  end
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    {guess_number, _} = Integer.parse(guess)
+
+    if guess_number == socket.assigns.number do
+      win(socket, guess)
+    else
+      lose(socket, guess)
+    end
+  end
+
+  def handle_params(_params, _addr, socket) do
+    {:noreply, default(socket)}
+  end
+
+  def reset(socket) do
+    {:noreply, default(socket)}
+  end
+
+  @spec render(any) :: Phoenix.LiveView.Rendered.t()
+  def render(assigns) do
+    ~H"""
+      <h1>Your score: <%= @score %></h1>
+      <h2>
+        <%= @message %><br>
+      </h2>
+      <h3>
+        Time: <%= @time %>
+      </h3>
+      <h2>
+        <%= for n <- 1..10 do %>
+          <.link href="#" phx-click="guess" phx-value-number={n} >
+            <%= n %>
+          </.link>
+        <% end %>
+      </h2>
+      <%= if @win do %>
+        <h1>
+          You Win!!!<br>
+          <span><%= live_patch "Reset", to: "/guess", reset: true %></span>
+        </h1>
+      <% end %>
+    """
+  end
+
+  def time() do
+    with time <- DateTime.utc_now() do
+      [time.hour, time.minute, time.second]
+      |> Enum.join(":")
+    end
+  end
+end
--- setup.sh
+++ setup.sh
@@ -1,34 +1,43 @@
 #!/usr/bin/env sh
 
+pg_is_down() {
+    pg_ctl status | grep -q "no server running"
+}
+
+pg_start() {
+    # start postgres
+    host_common="host\s\+all\s\+all"
+    sed -i "s|^$host_common.*127.*$|host all all 0.0.0.0/0 trust|" $PGDATA/pg_hba.conf
+    sed -i "s|^$host_common.*::1.*$|host all all ::/0 trust|" $PGDATA/pg_hba.conf
+    pg_ctl \
+        -D $PGDATA \
+        -l $PGDATA/postgres.log \
+        -o "-c unix_socket_directories='$PGDATA'" \
+        -o "-c listen_addresses='*'" \
+        -o "-c log_destination='stderr'" \
+        -o "-c logging_collector=on" \
+        -o "-c log_directory='log'" \
+        -o "-c log_filename='postgresql-%y-%m-%d_%h%m%s.log'" \
+        -o "-c log_min_messages=info" \
+        -o "-c log_min_error_statement=info" \
+        -o "-c log_connections=on" \
+        start
+}
+
 # initialize postgres if it's not created
 if ! test -d $PGDATA; then
     pg_ctl initdb -D $PGDATA
+    # if needs to change port
     # sed -i "s|^#port.*$|port = 5433|" $PGDATA/postgresql.conf
 else
+    pg_is_down && pg_start
     exit 0
 fi
 
 # Artifacts folder
 mkdir -p .nix-shell
 
-# start postgres
-host_common="host\s\+all\s\+all"
-sed -i "s|^$host_common.*127.*$|host all all 0.0.0.0/0 trust|" $PGDATA/pg_hba.conf
-sed -i "s|^$host_common.*::1.*$|host all all ::/0 trust|" $PGDATA/pg_hba.conf
-
-pg_ctl \
-    -D $PGDATA \
-    -l $PGDATA/postgres.log \
-    -o "-c unix_socket_directories='$PGDATA'" \
-    -o "-c listen_addresses='*'" \
-    -o "-c log_destination='stderr'" \
-    -o "-c logging_collector=on" \
-    -o "-c log_directory='log'" \
-    -o "-c log_filename='postgresql-%y-%m-%d_%h%m%s.log'" \
-    -o "-c log_min_messages=info" \
-    -o "-c log_min_error_statement=info" \
-    -o "-c log_connections=on" \
-    start
+pg_start
 
 # nodejs dependencies
 if test -d "$PWD/assets/" && ! test -d "$PWD/assets/node_modules/"; then
